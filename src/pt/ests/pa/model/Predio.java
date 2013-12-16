eeea6716f2e816c1117609ebe37010eb8bfe9c84
Rui Carvalho <astralenigma@hotmail.com>
2013-12-16 13:21:57 +0000

    Acrescentou-se o nº de pisos.

--- src/pt/ests/pa/model/Predio.java
+++ src/pt/ests/pa/model/Predio.java
@@ -4,15 +4,20 @@
  */
 package pt.ests.pa.model;
 
+import pt.ests.pa.model.tads.arraylist.ArrayList;
+import pt.ests.pa.model.tads.arraylist.ArrayListStatic;
+
 /**
  *
  * @author Rui
  */
 public class Predio {
     private static Predio instance;
-
+    private ArrayList<Piso> pisos;
+    private final int MAXPISOS=14;
     private Predio() {
         instance=new Predio();
+        pisos=new ArrayListStatic<Piso>(MAXPISOS);
     }
 
     public static Predio getInstance() {
