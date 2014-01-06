d7385a29efbcc4f10dac06b214b2d10f44578bd9
Rui Carvalho <astralenigma@hotmail.com>
2014-01-06 19:00:27 +0000

    Alterações incríveis no Piso.

--- src/pt/ests/pa/model/Piso.java
+++ src/pt/ests/pa/model/Piso.java
@@ -4,9 +4,12 @@
  */
 package pt.ests.pa.model;
 
+
 import pt.ests.pa.model.passageiro.Passageiro;
+import pt.ests.pa.model.tads.Iterator;
 import pt.ests.pa.model.tads.priorityqueue.PriorityQueue;
 import pt.ests.pa.model.tads.priorityqueue.PriorityQueueDynamic;
+import pt.ests.pa.model.tads.queue.Queue;
 
 /**
  * Classe dos pisos contem o numero do piso e os passageiros a espera.
@@ -16,7 +19,9 @@ import pt.ests.pa.model.tads.priorityqueue.PriorityQueueDynamic;
 public class Piso {
 
     private int nPiso;
-    private PriorityQueue<Passageiro> passageiros;
+    private PriorityQueue<Passageiro> passageirosASubir;
+    private PriorityQueue<Passageiro> passageirosADescer;
+    private Queue<Passageiro> passageirosAtendidos;
 
     /**
      * Constructor do piso recebe o numero do piso e gera a fila de passageiros.
@@ -25,20 +30,75 @@ public class Piso {
      */
     public Piso(int nPiso) {
         this.nPiso = nPiso;
-        passageiros = new PriorityQueueDynamic<>();
+        passageirosADescer = new PriorityQueueDynamic<>();
+        passageirosASubir = new PriorityQueueDynamic<>();
     }
 
     /**
      * Método usado para enviar os passageiros para o elevador.
      *
+     * @param aSubir O elevador esta a subir?
      * @return Passageiro que vai entrar no elevador.
      */
-    public Passageiro enviarPassageiro() {
-        return passageiros.dequeue();
+    public Passageiro enviarPassageiro(boolean aSubir) {
+        if (aSubir) {
+            return passageirosASubir.dequeue();
+        } else {
+            return passageirosADescer.dequeue();
+        }
+    }
+
+    /**
+     * Metodo que verifica a existencia de passageiros a descer.
+     *
+     * @return Devolve true se existerem passageiros na fila para descer.
+     */
+    public boolean existePassageiroDescer() {
+        return !passageirosADescer.isEmpty();
+    }
+
+    /**
+     * Metodo que verifica a existencia de passageiros a subir.
+     *
+     * @return Devolve true se existerem passageiros na fila para subir.
+     */
+    public boolean existePassageiroSubir() {
+        return !passageirosASubir.isEmpty();
+    }
+
+    /**
+     * Método usado para receber os passageiros do elevador.
+     *
+     * @param p
+     */
+    public void receberPassageiro(Passageiro p) {
+        passageirosAtendidos.enqueue(p);
+    }
+
+    /**
+     * Método usado para gerar os passageiros no piso.
+     *
+     * @param p Passageiro a ser criado.
+     */
+    public void gerarPassageiros(Passageiro p) {
+        if (p.getOrigem() > p.getDestino()) {
+            passageirosADescer.enqueue(p);
+        } else {
+            passageirosASubir.enqueue(p);
+        }
     }
 
     @Override
     public String toString() {
-        return  passageiros+"";
+        PriorityQueue<Passageiro> passsageiros= new PriorityQueueDynamic<>();
+        Iterator<Passageiro> it=passageirosADescer.getIterator();
+        while (it.hasNext()) {
+            passsageiros.enqueue(it.next());
+        }
+        it=passageirosASubir.getIterator();
+        while (it.hasNext()) {
+            passsageiros.enqueue(it.next());
+        }
+        return  passsageiros+ "";
     }
 }
