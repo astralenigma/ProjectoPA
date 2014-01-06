5d84d0ff5b16042c51da8ca042907f9e5385ba51
Rui Carvalho <astralenigma@hotmail.com>
2014-01-06 19:01:23 +0000

    Depois de muita luta já gera passageiros aleatoriamente (acho eu).

--- src/pt/ests/pa/controller/GestorDoPredio.java
+++ src/pt/ests/pa/controller/GestorDoPredio.java
@@ -4,11 +4,49 @@
  */
 package pt.ests.pa.controller;
 
+import java.util.Random;
+import pt.ests.pa.model.Predio;
+import pt.ests.pa.model.passageiro.ConcreteCreatorPassageiro;
+import pt.ests.pa.model.passageiro.Passageiro;
+
 /**
  *
  * @author Rui
  */
 public class GestorDoPredio {
-    //doSomething
+    private int nmrPisos = 8;
+    private int nmrElevadores = 2;
+    private int capacidadeElevador = 10;
+    private ConcreteCreatorPassageiro ccp;
+//    public static void criarPredio() throws QuantidadePisosIlegalException, QuantidadeElevadoresIlegalException, CapacidadeElevadorIlegalException {
+//        //doSomething
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Quantos Pisos tem o prédio?");
+//        int nmrPisos = sc.nextInt();
+//        if (7 > nmrPisos || nmrPisos > 15) {
+//            throw new QuantidadePisosIlegalException();
+//        }
+//        System.out.println("Quantos Elevadores tem o prédio?");
+//        sc.nextLine();
+//        int nmrElevadores = sc.nextInt();
+//        if (1 > nmrElevadores || nmrElevadores > 5) {
+//            throw new QuantidadeElevadoresIlegalException();
+//        }
+//        System.out.println("Qual a capacidade dos Elevadores do prédio?");
+//        sc.nextLine();
+//        int capacidadeElevador = sc.nextInt();
+//        if (capacidadeElevador < 1) {
+//            throw new CapacidadeElevadorIlegalException();
+//        }
+//    }
+
     
+    public void criarPassageiros() {
+        Random rd = new Random();
+        if (rd.nextBoolean()) {
+            Passageiro p= ccp.factoryMethod(rd.nextInt()%3, nmrPisos);
+            Predio.getInstance().gerarPassageiro(p.getOrigem(), p);
+        }
+    }
+    //public static void criarPredio()
 }
