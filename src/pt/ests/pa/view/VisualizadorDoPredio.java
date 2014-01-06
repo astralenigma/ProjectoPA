818ddfd42bdcc51e23afb1064d8f55c7b5023716
Rui Carvalho <astralenigma@hotmail.com>
2014-01-06 19:08:17 +0000

    Corrigiu-se o nome do VisualizadorDoPredio

---
+++ src/pt/ests/pa/view/VisualizadorDoPredio.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pt.ests.pa.view;
+
+import pt.ests.pa.model.Predio;
+import pt.ests.pa.model.exceptions.CapacidadeElevadorIlegalException;
+import pt.ests.pa.model.exceptions.QuantidadeElevadoresIlegalException;
+import pt.ests.pa.model.exceptions.QuantidadePisosIlegalException;
+
+/**
+ *
+ * @author Rui
+ */
+public class VisualizadorDoPredio {
+
+    public static void imprimirPredio() {
+        try {
+            System.out.println(Predio.getInstance());
+        } catch (QuantidadePisosIlegalException ex) {
+            System.out.println("Quantidade de pisos Ilegal.");
+        } catch (QuantidadeElevadoresIlegalException ex) {
+            System.out.println("Quantidade de elevadores Ilegal.");
+        } catch (CapacidadeElevadorIlegalException ex) {
+            System.out.println("Capacidade do elevador Ilegal");
+        }
+    }
+}
