63a354e3f5add0f5cfd974d83f65b7155830c214
Rui Carvalho <astralenigma@hotmail.com>
2014-01-08 18:07:32 +0000

    Passou-se tudo para o predio o Gestor passou a singleton.

--- src/pt/ests/pa/controller/GestorDoPredio.java
+++ src/pt/ests/pa/controller/GestorDoPredio.java
@@ -6,62 +6,41 @@ package pt.ests.pa.controller;
 
 import java.util.Observable;
 import java.util.Random;
+import pt.ests.pa.model.Elevador.Elevador;
 import pt.ests.pa.model.Predio;
 import pt.ests.pa.model.passageiro.ConcreteCreatorPassageiro;
 import pt.ests.pa.model.passageiro.Passageiro;
+import pt.ests.pa.model.tads.arraylist.ArrayList;
 
 /**
  *
  * @author Rui
  */
-public class GestorDoPredio extends Observable {
+public class GestorDoPredio  {
 
-    private static int nmrPisos;
-    private static int nmrElevadores;
-    private static int capacidadeElevador;
-    private ConcreteCreatorPassageiro ccp = new ConcreteCreatorPassageiro();
-
-    public GestorDoPredio(int nmrPisos, int nmrElevadores, int capacidadeElevador) {
-        GestorDoPredio.capacidadeElevador = capacidadeElevador;
-        GestorDoPredio.nmrElevadores = nmrElevadores;
-        GestorDoPredio.nmrPisos = nmrPisos;
-    }
-
-    /**
-     * Método que cria os passageiros.
-     */
-    public void criarPassageiros() {
-        Random rd = new Random();
-        if (rd.nextBoolean()) {
-            notifyObservers();
-            Passageiro p = ccp.factoryMethod(rd.nextInt(3), nmrPisos);
-            Predio.getInstance().gerarPassageiro(p.getOrigem(), p);
+    //singleton
+    private static GestorDoPredio instance;       
+    private Predio predio;     
+    
+    public static GestorDoPredio getInstance() {
+        if(instance == null) {
+            instance = new GestorDoPredio();
         }
+        return instance;
     }
 
     /**
-     *
+     * Cria o predio
+     * 
+     * @param nmrPisos
+     * @param nmrElevadores
+     * @param capacidadeElevador 
      */
-    public void accaoElevadores() {
-//        ArrayList<Elevador> al=Predio.getInstance().getElevadores();
-//        for (int i = 0; i < al.size(); i++) {
-//            al.get(i).;
-//        }
+    public void create(int nmrPisos, int nmrElevadores, int capacidadeElevador) {
+        predio = new Predio(nmrPisos, nmrElevadores, capacidadeElevador);
     }
 
-    public void criarPredio() {
-        Predio.getInstance();
-    }
-
-    public static int getNmrElevadores() {
-        return nmrElevadores;
-    }
-
-    public static int getCapacidadeElevador() {
-        return capacidadeElevador;
-    }
-
-    public static int getNmrPisos() {
-        return nmrPisos;
+    public Predio getPredio() {
+        return predio;
     }
 }
