3b7772a9456ac6042cee760214ae0da75762e0b1
Rui Carvalho <astralenigma@hotmail.com>
2014-01-08 13:34:21 +0000

    Removeu-se as perguntas das quantidades criou-se o constructor.

--- src/pt/ests/pa/controller/GestorDoPredio.java
+++ src/pt/ests/pa/controller/GestorDoPredio.java
@@ -6,15 +6,9 @@ package pt.ests.pa.controller;
 
 import java.util.Observable;
 import java.util.Random;
-import java.util.Scanner;
-import pt.ests.pa.model.Elevador.Elevador;
 import pt.ests.pa.model.Predio;
-import pt.ests.pa.model.exceptions.CapacidadeElevadorIlegalException;
-import pt.ests.pa.model.exceptions.QuantidadeElevadoresIlegalException;
-import pt.ests.pa.model.exceptions.QuantidadePisosIlegalException;
 import pt.ests.pa.model.passageiro.ConcreteCreatorPassageiro;
 import pt.ests.pa.model.passageiro.Passageiro;
-import pt.ests.pa.model.tads.arraylist.ArrayList;
 
 /**
  *
@@ -22,31 +16,16 @@ import pt.ests.pa.model.tads.arraylist.ArrayList;
  */
 public class GestorDoPredio extends Observable {
 
-    private static int nmrPisos = 8;
-    private static int nmrElevadores = 2;
-    private static int capacidadeElevador = 10;
+    private static int nmrPisos;
+    private static int nmrElevadores;
+    private static int capacidadeElevador;
     private ConcreteCreatorPassageiro ccp = new ConcreteCreatorPassageiro();
-//    public static void criarPredio() throws QuantidadePisosIlegalException, QuantidadeElevadoresIlegalException, CapacidadeElevadorIlegalException {
-//        //doSomething
-//        Scanner sc = new Scanner(System.in);
-//        System.out.println("Quantos Pisos tem o prédio?");
-//        int nmrPisos = sc.nextInt();
-//        if (7 > nmrPisos || nmrPisos > 15) {
-//            throw new QuantidadePisosIlegalException();
-//        }
-//        System.out.println("Quantos Elevadores tem o prédio?");
-//        sc.nextLine();
-//        int nmrElevadores = sc.nextInt();
-//        if (1 > nmrElevadores || nmrElevadores > 5) {
-//            throw new QuantidadeElevadoresIlegalException();
-//        }
-//        System.out.println("Qual a capacidade dos Elevadores do prédio?");
-//        sc.nextLine();
-//        int capacidadeElevador = sc.nextInt();
-//        if (capacidadeElevador < 1) {
-//            throw new CapacidadeElevadorIlegalException();
-//        }
-//    }
+
+    public GestorDoPredio(int nmrPisos, int nmrElevadores, int capacidadeElevador) {
+        GestorDoPredio.capacidadeElevador = capacidadeElevador;
+        GestorDoPredio.nmrElevadores = nmrElevadores;
+        GestorDoPredio.nmrPisos = nmrPisos;
+    }
 
     /**
      * Método que cria os passageiros.
@@ -71,24 +50,6 @@ public class GestorDoPredio extends Observable {
     }
 
     public void criarPredio() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Quantos Pisos tem o prédio?");
-        nmrPisos = sc.nextInt();
-        if (7 > nmrPisos || nmrPisos > 15) {
-            throw new QuantidadePisosIlegalException();
-        }
-        System.out.println("Quantos Elevadores tem o prédio?");
-        sc.nextLine();
-        nmrElevadores = sc.nextInt();
-        if (1 > nmrElevadores || nmrElevadores > 5) {
-            throw new QuantidadeElevadoresIlegalException();
-        }
-        System.out.println("Qual a capacidade dos Elevadores do prédio?");
-        sc.nextLine();
-        capacidadeElevador = sc.nextInt();
-        if (capacidadeElevador < 1) {
-            throw new CapacidadeElevadorIlegalException();
-        }
         Predio.getInstance();
     }
 
