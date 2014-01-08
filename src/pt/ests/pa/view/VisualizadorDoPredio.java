523d565d9f6a10bb980bf4d4760ac1ce874d5736
Rui Carvalho <astralenigma@hotmail.com>
2014-01-08 20:14:24 +0000

    Visualizador visualiza.

--- src/pt/ests/pa/view/VisualizadorDoPredio.java
+++ src/pt/ests/pa/view/VisualizadorDoPredio.java
@@ -4,15 +4,25 @@
  */
 package pt.ests.pa.view;
 
+import java.util.Observable;
+import java.util.Observer;
 import pt.ests.pa.model.Predio;
 
 /**
  *
  * @author Rui
  */
-public class VisualizadorDoPredio {
+public class VisualizadorDoPredio implements Observer {
 
-    public void imprimirPredio() {
-        System.out.println(Predio.getInstance());
+    @Override
+    public void update(Observable o, Object o1) {
+        
+        if(o instanceof Predio) {
+            Predio p = (Predio)o;
+            
+            System.out.println(p.toString());
+        } 
+            
+        
     }
 }
