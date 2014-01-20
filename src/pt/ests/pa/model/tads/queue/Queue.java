fa2133fccb629e062c8447f0a6efe8474b853e72
Rui Carvalho <astralenigma@hotmail.com>
2014-01-20 11:49:25 +0000

    Javadoc do Queue

--- src/pt/ests/pa/model/tads/queue/Queue.java
+++ src/pt/ests/pa/model/tads/queue/Queue.java
@@ -9,6 +9,7 @@ import pt.ests.pa.model.exceptions.EmptyQueueException;
 import pt.ests.pa.model.tads.Iterator;
 
 /**
+ * Usada para apoio ao projecto.
  *
  * @param <E> Classe do Objecto a ser inserido na Fila
  * @author Rui
@@ -16,41 +17,51 @@ import pt.ests.pa.model.tads.Iterator;
 public interface Queue<E> {
 
     /**
+     * Devolve o tamanho da fila.
      *
-     * @return
+     * @return Tamanho da Fila
      */
     public int size();
 
     /**
+     * Verifica se a fila está vazia
      *
-     * @return
+     * @return True se a fila estiver vazia e False se a fila estiver cheia.
      */
     public boolean isEmpty();
 
     /**
+     * Insere um elemento na fila.
      *
-     * @param elem
-     * @throws FullQueueException
+     * @param elem O elemento a ser inserido.
+     * @throws FullQueueException Excepção a ser atirada se a fila estiver
+     * cheia.
      */
     public void enqueue(E elem) throws FullQueueException;
 
     /**
+     * Remove um elemento da fila.
      *
-     * @return
-     * @throws EmptyQueueException
+     * @return O elemento a ser removido.
+     * @throws EmptyQueueException Excepção a ser atirada se a fila estiver
+     * vazia.
      */
     public E dequeue() throws EmptyQueueException;
 
     /**
+     * Espreita o próximo elemento a sair da fila.
      *
-     * @return
-     * @throws EmptyQueueException
+     * @return O elemento prestes a sair.
+     * @throws EmptyQueueException Excepção a ser atirada se a fila estiver
+     * cheia.
      */
     public E peek() throws EmptyQueueException;
-    
+
     /**
+     * Devolve o iterador da fila usado para mover-se na fila sem remover os
+     * elementos.
      *
-     * @return
+     * @return Iterador da fila.
      */
     public Iterator<E> getIterator();
 }
