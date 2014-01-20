0294f45b7d9a089a2c38d00ca19eb62593903730
Rui Carvalho <astralenigma@hotmail.com>
2014-01-20 16:50:59 +0000

    JavaFX da visualização do prédio.

--- src/pt/ests/pa/view/VisualizadorDoPredio.java
+++ src/pt/ests/pa/view/VisualizadorDoPredio.java
@@ -6,6 +6,11 @@ package pt.ests.pa.view;
 
 import java.util.Observable;
 import java.util.Observer;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import pt.ests.pa.model.Elevador.Elevador;
 import pt.ests.pa.model.Predio;
 
 /**
@@ -14,13 +19,64 @@ import pt.ests.pa.model.Predio;
  */
 public class VisualizadorDoPredio implements Observer {
 
+    @FXML
+    public VBox pisos;
+    public HBox elevadores;
+    public VBox pisosAtendidos;
+    public HBox informacaoElevadores;
+
+    public VisualizadorDoPredio() {
+    pisos=new VBox();
+    elevadores=new HBox();
+    pisosAtendidos=new VBox();
+    informacaoElevadores= new HBox();
+    }
+
+    
     @Override
     public void update(Observable o, Object o1) {
-        
+
         if (o instanceof Predio) {
             Predio p = (Predio) o;
-            
+//            clear();
+            writePisos(p);
+            writeElevadores(p);
             System.out.println(p.toString());
         }
+    }
+
+    public void writePisos(Predio p) {
+        for (int i = p.getMaxPisos() - 1; i >= 0; i--) {
+            pisos.getChildren().add(new Label(p.getPisos().get(i).toString()));
+            pisosAtendidos.getChildren().add(new Label(p.getPisos().get(i).passageirosAtendidos()));
+        }
+    }
+
+    public void writeElevadores(Predio p) {
+        for (int i = p.getNmrElevadores() - 1; i >= 0; i--) {
+            Elevador e = p.getElevadores().get(i);
+            elevadores.getChildren().add(drawElevador(p, e));
+            String str = "";
+            str += ("Numero do elevador: " + (i + 1) + "\n");
+            str += ("Numero de passageiros no elevador: " + e.getNmrPassageiros() + "\n");
+            str += ("Estado do Elevador: " + e.getEstado().palavras() + "\n");
+            str += ("Distancia Percorrida: " + e.getnPisosPercorridos() + "\n");
+            informacaoElevadores.getChildren().add(new Label(str));
+        }
+    }
+
+    public VBox drawElevador(Predio p, Elevador e) {
+        VBox v = new VBox();
+        for (int i = p.getMaxPisos() - 1; i >= 0; i--) {
+            v.getChildren().add(new Label((e.getnumPisoActual() == i) ? (e.toString()) : ""));
+        }
+        return v;
+    }
+
+    public void clear() {
+        elevadores.getChildren().clear();
+        informacaoElevadores.getChildren().clear();
+        pisos.getChildren().clear();
+        pisosAtendidos.getChildren().clear();
     }
 }
